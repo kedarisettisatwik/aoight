9fc4de4872da3a6bbb5d73cf100b2f049dd0f28c
kedarisettisatwik <74258680+kedarisettisatwik@users.noreply.github.com>
2022-01-19 14:35:29 +0530

    De

--- src/index.js
+++ src/index.js
@@ -2,13 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-
+import {BrowserRouter} from 'react-router-dom';
 import App from './App';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App/>
-  </React.StrictMode>,
+  <BrowserRouter>
+     <App />
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
