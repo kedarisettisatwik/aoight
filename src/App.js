1c1792cc80597a598340fff22afae5d84e72992c
kedarisettisatwik <74258680+kedarisettisatwik@users.noreply.github.com>
2022-01-20 21:44:36 +0530

    De

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 
 import React from 'react';
 import {Routes,Route,useLocation} from 'react-router-dom';
-import { AnimatePresence } from 'framer-motion';
 import Intro from './Intro';
 import Dash from './Pages/dashboard/Dashboard';
 import Error  from './Pages/error/Error';
@@ -11,14 +10,12 @@ function App(){
   const location = useLocation();
 
   return (
-    <AnimatePresence>
     <Routes location={location} key={location.pathname}>
        <Route path="/" exact element={<Intro />} />
        <Route path="/aoight-start" exact element={<Intro />} />
        <Route path="/aoight-dashboard" exact element={<Dash />} />
        <Route path="*" exact element={<Error />} />
     </Routes>
-    </AnimatePresence>
   );
 }
 
