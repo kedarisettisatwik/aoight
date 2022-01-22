1f6c62037335076592ecd5e4bcb74a57f06c9aed
kedarisettisatwik <74258680+kedarisettisatwik@users.noreply.github.com>
2022-01-22 15:00:39 +0530

    de

--- src/Pages/new/New.js
+++ src/Pages/new/New.js
@@ -41,29 +41,58 @@ const Box2 = () => {
 
   const [classi,setClassi] = useState('input');
 
-  const list = ['https://lh3.googleusercontent.com/a-/AOh14GiS1xDkL47MBrHjTGcHtWrR-VkjdGAlO9Dt-mZPsw=s96-c-rg-br100','satwik kedar','satwik1330@gamil.com',"India"];
+  const list = ['https://lh3.googleusercontent.com/a-/AOh14GiS1xDkL47MBrHjTGcHtWrR-VkjdGAlO9Dt-mZPsw=s96-c-rg-br100','satwik kedar','satwik1330@gamil.com',"India","public"];
 
   const [details,setDetails] = useState(list);
 
   const Change1 = (e) => {
-    setDetails([details[0],e.target.value,details[2],details[3]]);
+    setDetails([details[0],e.target.value,details[2],details[3],details[4]]);
  }
 
  const Blur1 = (e) => {
   if (e.target.value.length === 0){
-    setDetails([details[0],list[1],details[2],details[3]]);
+    setDetails([details[0],list[1],details[2],details[3],details[4]]);
   }
   setClassi('input');
 }
 
 const Country1 = (val) => {
   if (val === ''){
-    setDetails([details[0],details[1],details[2],list[3]]);
+    setDetails([details[0],details[1],details[2],list[3],details[4]]);
   }else{
-    setDetails([details[0],details[1],details[2],val]);
+    setDetails([details[0],details[1],details[2],val,details[4]]);
   }
 }
 
+const Cat1 = (e) => {
+  var l = e.target.value;
+  setDetails([details[0],details[1],details[2],details[3],l]);
+}
+
+var list1 = ["Arts","Athelete","Author","Advertising",
+             "Bussiness","Book","Beauty","Bar",
+             "College","community","cloths","cooking","contractor","camera","commercial",
+             "Design & Fashion","DJ","Dancer","Digital creator","Doctor","Driving","Developer",
+             "Education","enterpreneur","e-commerce","entertainment","electronics","electrical","editor","engineering",
+             "food","financial","Fashion","farm","furniture",
+             "games","gym","grocery","graphics","government","garden","gift",
+             "health","hotel","hospital","hardware",
+             "IT","industry","ice cream","internet","insurance",
+             "jewelry","journalist",
+             "kitchen",
+             "library",
+             "Music","movie","marketing","medical","media","magazine",
+             "news","NGO","nursing",
+             "office",
+             "photographer","politician","pharmacy","pizza","public","professor",
+             "Real Estate","resturant","radio","retial",
+             "school","song","shopping","sports","social","solider","security","software",
+             "travel","tv","train","tax",
+             "urban","uniform",
+             "website desinger","water","wine",
+             "Xerox",
+             "Yoga",
+             "zoo"];
 return (
       <div className='box2 active'>
             <p><strong>Hello,</strong> we are very Happy to see you here, 
@@ -89,6 +118,15 @@ return (
            <div className='input'>
              <label> Country </label>
              <CountryDropdown value={details[3]} onChange={(val) => {Country1(val)}}/>
+             <i className="fas fa-pen options"></i>
+           </div>
+           <h4> What describes you the best ? </h4>
+           <div className='input' style={{"margin":"10px 0"}}>
+             <select value={details[4]} onChange={(e) => {Cat1(e)} }>
+                {
+                  list1.map(item => <option value={item}>{item}</option>)
+                }
+             </select>
              <i className="fas fa-pen options"></i>
            </div>
            <div className='buttons flex'>
