d8d74e718189956a5c0cd4f9ed611b2bbbc3b792
kedarisettisatwik <74258680+kedarisettisatwik@users.noreply.github.com>
2022-01-23 10:47:24 +0530

    de

--- src/Intro.js
+++ src/Intro.js
@@ -40,17 +40,16 @@ function Intro() {
         // console.log(user_data);
         // sessionStorage.setItem("user", JSON.stringify(user_data));
 
-        // const diff = Math.abs(user.metadata.lastLoginAt - user.metadata.createdAt);
+        const diff = Math.abs(user.metadata.lastLoginAt - user.metadata.createdAt);
 
-        // console.log(user);
+        console.log(user);
         
-        // if (diff < 1000 ){
-        //   navigate('/aoight-new',{state:user_data,replace:true});
-        // }else{
-        //   navigate('/aoight-dashboard',{state:user_data,replace:true});
-        // }
+        if (diff < 1000 ){
+          navigate('/aoight-new',{state:user_data,replace:true});
+        }else{
+          navigate('/aoight-dashboard',{state:user_data,replace:true});
+        }
 
-        navigate('/aoight-new',{state:user_data,replace:true});
 
       }).catch((error) => {
         console.log(error);
