5b175a12b402ea48317dda60869d25b4e0caaa7d
kedarisettisatwik <74258680+kedarisettisatwik@users.noreply.github.com>
2022-01-24 14:16:29 +0530

    De

--- src/Pages/dashboard/Dashboard.js
+++ src/Pages/dashboard/Dashboard.js
@@ -1,34 +1,69 @@
-import React from 'react';
+import React, { useState ,useEffect }from 'react';
 import { useLocation } from 'react-router-dom';
 import Error from '../error/Error';
 import './dash.css';
-// import base from "../../firebase";
-// import { collection,getDocs, doc, getDoc } from 'firebase/firestore/lite';
+import base from "../../firebase";
+import { collection,getDocs, doc, getDoc } from 'firebase/firestore/lite';
 
 const Ele = () => {
 
-  // const location = useLocation();
-  // if (location.state){
-  //   return (
-  //     <h2>Old User</h2>
-  //   );
-  // }else{
-  //   return (<Error/>);
-  // }
+  const location = useLocation();
 
-  return (
-    <h2>old user </h2>
-  );
+  if (location.state){
+    return (<Aap/>);
+  }else{
+    return (<Error/>);
+  }
 
 }
 
+function Aap(){
 
+  const location = useLocation();
+
+  const [items,setItems] = useState({});
+  const [loading,setLoading] = useState(true);
+  
+  async function getvalues(){
+    setLoading(true);
+    const docRef = doc(base,"users",location.state.uid,"profile","visible");
+    const docSnap = await getDoc(docRef);
+     if (docSnap.exists()) {
+            const ob = docSnap.data();
+            console.log(ob);
+            var li = {"Name":ob.Name,"Photo":ob.Photo,"Email":ob.Email,"Tag":ob.Tag,"Uid":ob.Uid};
+            setItems(li);
+            setLoading(false);
+            // console.log("Document data:", list);
+        } else {
+    // doc.data() will be undefined in this case
+           console.log("No such document!");
+       }
+  }
+
+  useEffect(() => {
+    getvalues();
+  },{});
+
+  if(!loading){
+    return (
+      <>
+      <img alt='pro' src={items.Photo}></img>
+      <h3>HI Mr. {items.Name} - {items.Tag}</h3>
+      <p> {items.Email} <br></br> </p>
+      </>
+    );
+  }else{
+    return (<h2>Hello user</h2>);
+  }
+
+}
 
 function Board() {
 
   return(
     <>
-    <div className='flex dash'>
+    <div className='dash'>
           <Ele/>
     </div>
     </>
