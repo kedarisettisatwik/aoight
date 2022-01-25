38922b8210c9a374b6b0299d581e7fcb73af884e
kedarisettisatwik <74258680+kedarisettisatwik@users.noreply.github.com>
2022-01-25 14:14:57 +0530

    de

--- src/Pages/dashboard/Dashboard.js
+++ src/Pages/dashboard/Dashboard.js
@@ -1,12 +1,48 @@
 import React, { useState ,useEffect }from 'react';
-import { useLocation } from 'react-router-dom';
+import { useLocation, useNavigate} from 'react-router-dom';
 import Error from '../error/Error';
 import './dash.css';
 import base from "../../firebase";
 import { collection,getDocs, doc, getDoc } from 'firebase/firestore/lite';
-import { BrowserView, MobileView, isBrowser, isMobile } from 'react-device-detect';
 
-const Ele = () => {
+function Aap(){
+
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  const [items,setItems] = useState({});
+  
+  async function getvalues(){
+
+    const docRef = doc(base,"users",location.state.Uid,"profile","visible");
+    const docSnap = await getDoc(docRef);
+     if (docSnap.exists()) {
+            const ob = docSnap.data();
+            // console.log(ob);
+            var li = {"Name":ob.Name,"Photo":ob.Photo,"Email":ob.Email,"Tag":ob.Tag,"Uid":ob.Uid};
+            setItems(li);
+        } else {
+           //  console.log("No such document!");
+           navigate('/aoight',{replace:'true'});
+       }
+
+  }
+
+  getvalues();
+
+  return(
+    <>
+       <div className='flex dash'>
+         <div className='side'>
+           <h3>HEllo</h3>
+         </div>
+       </div>
+    </>
+  )
+
+}
+
+function Board() {
 
   const location = useLocation();
 
@@ -15,65 +51,6 @@ const Ele = () => {
   }else{
     return (<Error/>);
   }
-
-}
-
-function Aap(){
-
-  const location = useLocation();
-
-  const [items,setItems] = useState({});
-  const [loading,setLoading] = useState(true);
-  
-  async function getvalues(){
-
-    setLoading(true);
-    const docRef = doc(base,"users",location.state.Uid,"profile","visible");
-    const docSnap = await getDoc(docRef);
-     if (docSnap.exists()) {
-            const ob = docSnap.data();
-            // console.log(ob);
-            var li = {"Name":ob.Name,"Photo":ob.Photo,"Email":ob.Email,"Tag":ob.Tag,"Uid":ob.Uid};
-            setItems(li);
-            setLoading(false);
-            // console.log("Document data:", list);
-        } else {
-    // doc.data() will be undefined in this case
-           console.log("No such document!");
-       }
-       
-  }
-
-  useEffect(() => {
-    getvalues();
-  },{});
-
-  if(!loading){
-    return (
-      <>
-      <BrowserView>
-               <h2>Seeing in Laptop</h2>
-      </BrowserView>
-      <MobileView>
-               <h2>Seeing in Mobile</h2>
-      </MobileView>
-      </>
-    );
-  }else{
-    return (<h2>Hello user</h2>);
-  }
-
-}
-
-function Board() {
-
-  return(
-    <>
-    <div className='dash'>
-          <Ele/>
-    </div>
-    </>
-  );
 
 }
 
--- src/Pages/load/Load.js
+++ src/Pages/load/Load.js
@@ -1,6 +1,8 @@
 import React,{ useState ,useEffect} from 'react';
 import Load_img from './loading.png';
+import '../../index.css';
 import { useLocation, useNavigate } from 'react-router-dom';
+import {isBrowser, isMobile } from 'react-device-detect';
 
 import base from "../../firebase";
 import { doc , getDoc } from 'firebase/firestore/lite';
@@ -8,6 +10,7 @@ import { doc , getDoc } from 'firebase/firestore/lite';
 function Error(){
     const location = useLocation();
     const navigate = useNavigate();
+    const width1 = window.innerWidth;
 
     const style1 = {"position":"relative","width":"100vw","height":"100vh","display":"flex","justifyContent":"center","alignItems":"center"};
     
@@ -19,7 +22,15 @@ function Error(){
         const docSnap = await getDoc(docRef);
          if (docSnap.exists()) {
              setTimeout(() => {
-              navigate('/aoight-dashboard',{state:location.state,replace:true});
+               if (isMobile){
+                 if (width1 > 800){
+                   // tablet size
+                 }else{
+                   // mobile size
+                 }
+               }else if (isBrowser){
+                navigate('/aoight-dashboard',{state:location.state,replace:true});
+               }
              }, 1000);
             } else {
               // console.log("New user");
