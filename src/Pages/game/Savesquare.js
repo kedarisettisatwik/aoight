58b2db64ac35a56d774bf91cb1ac6e629831b6b0
kedarisettisatwik <74258680+kedarisettisatwik@users.noreply.github.com>
2022-05-02 13:12:12 +0530

    De

--- src/Pages/game/Savesquare.js
+++ src/Pages/game/Savesquare.js
@@ -1,13 +1,20 @@
 import React from 'react';
+import {isMobile } from 'react-device-detect';
 // import { useNavigate } from 'react-router-dom';
 
 function Savesquare() {
     // const navigate = useNavigate();
     // navigate('/asdfasf');
     const style1 = {"position":"relative","outline":"none","border":"none","width":"100vw","height":"100vh","display":"flex","justifyContent":"center","alignItems":"center"};    
-    return (
-    <iframe title='game' src='https://kedarisettisatwik.github.io/square/rules.html' style={style1}></iframe>
-    );
+    if (isMobile){
+        return (
+            <iframe title='game1' src='https://kedarisettisatwik.github.io/square/rules.html?mobile=1' style={style1}></iframe>
+            );
+    }else{
+        return (
+            <iframe title='game2' src='https://kedarisettisatwik.github.io/square/rules.html?mobile=0' style={style1}></iframe>
+            );
+    }
 }
 
 export default Savesquare;
