a683e6f3382095fd71f0cdea6bc5bc09b07221d1
kedarisettisatwik <74258680+kedarisettisatwik@users.noreply.github.com>
2022-07-03 21:03:53 +0530

    De

--- src/App.js
+++ src/App.js
@@ -1,11 +1,7 @@
 
 import React from 'react';
 import {Routes,Route,useLocation} from 'react-router-dom';
-import Intro from './Intro';
-import Dash from './Pages/dashboard/Dashboard';
 import Error  from './Pages/error/Error';
-import New from './Pages/new/New';
-import Load from './Pages/load/Load';
 import Savesquare from './Pages/game/Savesquare';
 
 function App(){
