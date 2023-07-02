2c37999f276a1e21479c1ba526223586be153530
kedarisettisatwik <74258680+kedarisettisatwik@users.noreply.github.com>
2023-07-02 08:43:32 +0530

    De

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import {Routes,Route} from 'react-router-dom';
 import Error  from './Pages/error/Error';
 import VibeChats from './Pages/vibeTalk/VibeChats';
-import SignVibeTalk from './Pages/vibeTalk/SignVibeChats';
 import Website from './Pages/website/Website';
 
 function App(){
@@ -11,7 +10,6 @@ function App(){
     <Routes>
       <Route path="/" element={<Website />} />
       <Route path="/vibeChats" element={<VibeChats/>}></Route>
-      <Route path="/signup" element={<SignVibeTalk/>}></Route>
       <Route path="*" element={<Error/>}/>
     </Routes>
   );
--- src/Pages/vibeTalk/VibeChats.js
+++ src/Pages/vibeTalk/VibeChats.js
@@ -1,5 +1,4 @@
-import React, { useState } from 'react';
-import { useNavigate   } from 'react-router-dom';
+import React from 'react';
 import VClogo from './images/videChatLogo.png';
 import './styles/loginPage.css';
 
@@ -20,56 +19,16 @@ const signInWithGoogle = () => {
 };
 
 
-function VibeChats({history}){
+function VibeChats(){
 
-    const navigate = useNavigate();
-    const [passwordVisible, setPasswordVisible] = useState(false);
-
-    const signUp = () => {
-        navigate('/signup');
-    };
-
-    const togglePasswordVisibility = () => {
-        setPasswordVisible(!passwordVisible);
-    };
-
-    const [mailInput, setMailInput] = useState('');
-    const [passInput, setPassInput] = useState('');
-
-    const handleMailInputChange = (e) =>{
-        setMailInput(e.target.value);
-    };
-
-    const handlePassInputChange = (e) => {
-        setPassInput(e.target.value);
-    }
-    
-    const registerForm = (e) => {
-        e.preventDefault();
-        console.log(mailInput);
-        console.log(passInput);
-    }
-    
     return(
         <section className="mainPage login">
             <img src={VClogo} alt='logo' className='logo'></img>
-            <form id='LogInForm' onSubmit={registerForm}> 
+            <div id='LogInForm'> 
                 <h2>VibeChats</h2>
-                <span>Email Address</span>
-                <input type='email' placeholder='enter email @gmail.com' required value={mailInput} onChange={handleMailInputChange}></input>
-                <span>Password</span>
-                <div className={`password ${passwordVisible ? 'active' : ''}`} style={{position:'relative'}}>
-                    <input type={passwordVisible ? 'text' : 'password'} required placeholder={`${passwordVisible ? 'secret key' : '****** ***'}`} value={passInput} onChange={handlePassInputChange}></input>
-                    <i className="fa-solid fa-eye eyeOpen" onClick={togglePasswordVisibility}></i>
-                    <i className="fa-solid fa-eye-slash eyeClose" onClick={togglePasswordVisibility}></i>
-                </div>
-                <input type='submit' value='Continue to Chat'></input>
-                <button onClick={signInWithGoogle} className='googleBtn'>Use Google Account</button>
-                <div className='up flex'>
-                    <p>Not Yet Signed Up ?</p>
-                    <label onClick={signUp}>SignUp Now</label>
-                </div>
-            </form>
+                <span>Continue to Chat</span>
+                <button onClick={signInWithGoogle} className='googleBtn'>Log in With Google </button>
+            </div>
         </section>
     )
 }
